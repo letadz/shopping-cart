bc98088e4fdce86c65a859df176c8873239634ea
Letadz <mletada@traxiontech.net>
2024-03-13 14:10:39 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <>
+      <div>
+        <span className="text-9xl">Hello</span>
+      </div>
+    </>
+  );
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./main.jsx", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
